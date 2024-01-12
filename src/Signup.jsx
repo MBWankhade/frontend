ee168ed4d47e0ea8eee50c4f526d309d035f1225
MBWankhade <99143992+MBWankhade@users.noreply.github.com>
2024-01-12 14:14:35 +0530

    Update Signup.jsx

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { FacebookLoginButton, GoogleLoginButton } from 'react-social-login-buttons';
+import React, { useState } from 'react';
+import { GoogleLoginButton, FacebookLoginButton } from 'react-social-login-buttons';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const backendURL = 'https://backend-daily.onrender.com';
 
 const Signup = () => {
   const navigate = useNavigate();
-  const [email, setEmail] = React.useState('');
-  const [name, setName] = React.useState('');
-  const [password, setPassword] = React.useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleSignup = () => {
     fetch(`${backendURL}/admin/signup`, {
@@ -23,77 +23,71 @@ const Signup = () => {
         password: password,
       }),
     })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
+      .then(response => response.json())
       .then(data => {
-        localStorage.setItem('token', data.token);
-        navigate('/');
+        console.log(data);
+        if (data.message === 'Admin created successfully') {
+          navigate('/');
+          localStorage.setItem('token', data.token);
+        } else {
+          alert(data.message);
+        }
       })
       .catch(error => {
         console.error('Error:', error);
       });
   };
 
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', overflow: 'hidden', backgroundColor: '#F0F4F8' }}>
-      <div style={{ width: '80%', display: 'flex', borderRadius: '16px', overflow: 'hidden', boxShadow: '0px 0px 20px 0px rgba(0, 0, 0, 0.1)' }}>
-        {/* Left Section */}
-        <div style={{ flex: '1', background: 'linear-gradient(135deg, #64B5F6, #4CAF50)', padding: '50px', display: 'flex', flexDirection: 'column', alignItems: 'center', overflow: 'hidden', borderRadius: '16px 0 0 16px' }}>
-          <div style={{ color: '#FFF', fontFamily: 'Montserrat', fontSize: '24px', fontWeight: 600, textAlign: 'center', marginBottom: '20px' }}>
-            Getting Started with VR Creation
-          </div>
-          <img src="static/images/Abstraction.png" alt="Abstraction" style={{ width: '100%', marginTop: '20px', transform: 'rotate(-9.253deg)', borderRadius: '8px' }} />
-        </div>
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#4CAF50',
+      },
+    },
+  });
 
-        {/* Right Section */}
-        <div style={{ flex: '1', background: '#FFFFFF', padding: '50px', display: 'flex', flexDirection: 'column', alignItems: 'center', overflowY: 'hidden' }}>
-          <div style={{ color: '#333', fontFamily: 'Montserrat', fontSize: '32px', fontWeight: 800, marginBottom: '30px', textAlign: 'center' }}>
-            Create Your Account
-          </div>
-          <div style={{ display: 'flex', justifyContent: 'space-between', width: '80%', marginBottom: '30px' }}>
-            <div style={{ width: '48%' }}>
-              <GoogleLoginButton onClick={() => {}} />
+  return (
+    <ThemeProvider theme={theme}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', background: 'linear-gradient(to right, #86BFAF, #4CAF50)' }}>
+        <div style={{ width: '400px', borderRadius: '16px', overflow: 'hidden', boxShadow: '0px 0px 20px 0px rgba(0, 0, 0, 0.1)' }}>
+          {/* Right Section */}
+          <div style={{ background: '#FFFFFF', padding: '40px', display: 'flex', flexDirection: 'column', alignItems: 'center', borderRadius: '16px 0px 0px 16px' }}>
+            <div style={{ color: '#4CAF50', fontFamily: 'Montserrat', fontSize: '32px', fontWeight: 800, marginBottom: '20px', textAlign: 'center', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.1)' }}>
+              Create Your Account
             </div>
-            <div style={{ width: '48%' }}>
-              <FacebookLoginButton onClick={() => {}} />
-            </div>
+            <form style={{ width: '100%' }}>
+              <TextField label="Email" variant="outlined" fullWidth type="email" onChange={e => setEmail(e.target.value)} style={{ marginBottom: '20px' }} InputProps={{ style: { borderRadius: '5px' } }} />
+              <TextField label="Password" variant="outlined" fullWidth type="password" onChange={e => setPassword(e.target.value)} style={{ marginBottom: '20px' }} InputProps={{ style: { borderRadius: '5px' } }} />
+              <Button
+                onClick={handleSignup}
+                variant="contained"
+                fullWidth
+                sx={{
+                  background: '#4CAF50',
+                  color: '#FFF',
+                  height: '50px',
+                  borderRadius: '5px',
+                  fontSize: '18px',
+                  fontWeight: 800,
+                  boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+                  transition: 'background 0.3s',
+                  '&:hover': {
+                    background: '#45a749',
+                  },
+                }}
+              >
+                Create Account
+              </Button>
+            </form>
+            <p style={{ marginTop: '20px', color: '#333', fontSize: '14px' }}>
+              Already have an account? <span style={{ cursor: 'pointer', color: '#4CAF50', fontWeight: 'bold' }} onClick={() => navigate('/login')}>Login</span>
+            </p>
           </div>
-          <div style={{ color: '#666', fontFamily: 'Montserrat', fontSize: '18px', fontWeight: 500, marginBottom: '20px', textAlign: 'center' }}>
-            - OR -
-          </div>
-          <div style={{ width: '80%', marginBottom: '30px' }}>
-            <TextField id="outlined-basic" label="Full Name" variant="outlined" fullWidth onChange={e => setName(e.target.value)} />
-            <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth type="email" onChange={e => setEmail(e.target.value)} style={{ marginTop: '20px' }} />
-            <TextField id="outlined-basic" label="Password" variant="outlined" fullWidth type="password" onChange={e => setPassword(e.target.value)} style={{ marginTop: '20px' }} />
-          </div>
-          <Button
-            onClick={handleSignup}
-            variant="contained"
-            style={{
-              width: '80%',
-              height: '57px',
-              borderRadius: '5px',
-              background: '#4CAF50',
-              color: '#FFF',
-              fontFamily: 'Montserrat',
-              fontSize: '18px',
-              fontWeight: 800,
-              marginTop: '20px',
-            }}
-          >
-            Create Account
-          </Button>
-          <button onClick={()=>{
-            navigate('/login')
-          }}>Already have an account?Login</button>
         </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 };
 
 export default Signup;
+
